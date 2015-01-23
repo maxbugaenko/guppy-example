910ff12912f64e65692ba87d51dae8cab8ed8ce7
Max Bugaenko <max.bugaenko@gmail.com>
2015-01-23 17:03:24 +0200

    finita

--- example.rb
+++ example.rb
@@ -4,6 +4,10 @@ require 'haml'
 require 'haml/helpers'
 require 'guppy'
 
+# as you see we just required guppy gem
+# from now on you can @import guppy
+# modules from your scss scripts
+
 get '/' do
   haml :index, layout: :layout
 end
